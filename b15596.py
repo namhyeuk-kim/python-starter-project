5d68e138fdb508cedf610149faf56fbfc2887cb0
namhyeuk-kim <100276154+namhyeuk-kim@users.noreply.github.com>
2022-03-06 17:17:35 +0900

    백준 단계별 함수

---
+++ b15596.py
@@ -0,0 +1,3 @@
+def solve(n):
+  return sum(n)
+  
---
+++ b4673.py
@@ -0,0 +1,12 @@
+def self(n):
+    n = n + sum(map(int, str(n)))
+    return n
+
+
+non_self_num = set()
+for i in range(1, 10001):
+    non_self_num.add(self(i))
+
+for j in range(1, 10001):
+    if j not in non_self_num:
+        print(j)
